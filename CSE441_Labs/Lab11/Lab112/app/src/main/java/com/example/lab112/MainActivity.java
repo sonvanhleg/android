acc79a139bbb4b1abcdb089914c129e938d88f53
sonvanhleg <sona3k19hbt@gmail.com>
2025-05-28 11:59:45 +0700

    lab11

---
+++ CSE441_Labs/Lab11/Lab112/app/src/main/java/com/example/lab112/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.lab112;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        Intent intent1 = getIntent();
+        Uri data = intent1.getData();
+        try {
+            WebView webView = (WebView) findViewById(R.id.main);
+            webView.loadUrl(data.toString());
+        }catch (Exception  e){
+            e.printStackTrace();
+        }
+    }
+}
